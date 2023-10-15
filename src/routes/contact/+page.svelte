2fb52a7ba9e03d7f9c63ef843772353ec05bd6f6
Jonathan Jayes <j0nathanjayes@gmail.com>
2023-10-15 12:12:56 +0200

    add about page

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { onMount } from 'svelte';
+
+    let posts = [];
+
+    onMount(async () => {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        posts = await response.json();
+    });
+</script>
+
+<ul>
+    {#each posts as post (post.id)}
+        <li>{post.title}</li>
+    {/each}
+</ul>
