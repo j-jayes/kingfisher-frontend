8a362e5cda2a9c2d37a79a45c6702208111f3336
Jonathan Jayes <j0nathanjayes@gmail.com>
2023-10-16 19:29:54 +0200

    change map

--- src/lib/MapComponent.svelte
+++ src/lib/MapComponent.svelte
@@ -3,16 +3,15 @@
     import { onMount } from 'svelte';
   
     let map;
-  
     onMount(() => {
-      map = L.map('map').setView([49.1833, -2.1006], 13); // Coordinates for St Helier, Jersey
+      map = L.map('map').setView([49.4482, -2.5895], 13); // Coordinates for Guernsey
   
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       }).addTo(map);
   
       L.marker([49.1833, -2.1006]).addTo(map)
-        .bindPopup('St Helier, Jersey')
+        .bindPopup('Guernsey')
         .openPopup();
     });
 </script>
