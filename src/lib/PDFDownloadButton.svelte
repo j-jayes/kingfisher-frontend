3a280cb27219b96829da04fb5f247845bc7f8236
Jonathan Jayes <j0nathanjayes@gmail.com>
2023-11-27 20:01:24 +0200

    add new PDF downloader

--- src/lib/PDFDownloadButton.svelte
+++ src/lib/PDFDownloadButton.svelte
@@ -1,8 +1,4 @@
 <script>
-	import { onMount } from 'svelte';
-	import { jsPDF } from 'jspdf';
-	import html2canvas from 'html2canvas';
-
 	export let pdfContainer;
 	export let loadingPDF;
 	export let queryString; // Accept the queryString prop
@@ -15,33 +11,33 @@
 		const pdfContent = `<div class="pdf-header">${headerContent}</div>` + pdfContainer.innerHTML;
 		const css = extractCSS();
 
-		const response = await fetch('/generate-pdf', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({ html: pdfContent, css })
-		});
+		try {
+			const response = await fetch('https://us-central1-kingfisher-backend.cloudfunctions.net/generatePDF', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({ html: pdfContent, css })
+			});
 
-		// Handle the PDF download
-		const blob = await response.blob();
-		const url = window.URL.createObjectURL(blob);
+			if (response.ok) {
+				const blob = await response.blob();
+				const url = window.URL.createObjectURL(blob);
+				const filename = `Alcedo-${queryString}.pdf`; // Ensure queryString is safe to use
 
-		// Set the filename using the query string
-		const filename = `Alcedo-${queryString}.pdf`; // Ensure queryString is safe to use
-
-		// Create a temporary anchor element for the download
-		const a = document.createElement('a');
-		a.href = url;
-		a.download = filename;
-
-		// Append the anchor to the body, trigger click, and then remove it
-		document.body.appendChild(a);
-		a.click();
-		document.body.removeChild(a);
-
-		// Optional: Revoke the blob URL to free up resources
-		window.URL.revokeObjectURL(url);
-
-		loadingPDF = false;
+				const a = document.createElement('a');
+				a.href = url;
+				a.download = filename;
+				document.body.appendChild(a);
+				a.click();
+				document.body.removeChild(a);
+				window.URL.revokeObjectURL(url);
+			} else {
+				console.error('Error:', await response.text());
+			}
+		} catch (error) {
+			console.error('Error downloading PDF:', error);
+		} finally {
+			loadingPDF = false;
+		}
 	}
 
 	function extractCSS() {
@@ -55,46 +51,42 @@
 				console.warn('Could not access stylesheet: ', sheet.href);
 			}
 		}
+		// Extract inline styles
+		document.querySelectorAll('style').forEach(style => {
+			styles += style.innerHTML;
+		});
 		return styles;
 	}
 
 	function sanitizeFilename(filename) {
 		return filename
-			.replace(/[/\\?%*:|"<>]/g, '-') // Replacing invalid characters with '-'
-			.replace(/(\.\.\/)|(\/\.\.)/g, '') // Removing directory traversal attempts
-			.substring(0, 255); // Limiting length to 255 characters
+			.replace(/[/\\?%*:|"<>]/g, '-')
+			.replace(/(\.\.\/)|(\/\.\.)/g, '')
+			.substring(0, 255);
 	}
 
 	function formatDateAndQueryString(queryString) {
-		// Format the current date and time
 		const now = new Date();
 		const dateStr = now.toLocaleDateString('en-GB', {
-			year: 'numeric',
-			month: '2-digit',
-			day: '2-digit'
+			year: 'numeric', month: '2-digit', day: '2-digit'
 		});
 		const timeStr = now.toLocaleTimeString('en-US', {
-			hour: '2-digit',
-			minute: '2-digit'
+			hour: '2-digit', minute: '2-digit'
 		});
-
-		// Format the query string
 		let queryParamsText = `Search Query:\n${queryString.replace(/&/g, '\n').replace(/=/g, ': ')}`;
-
-		// Return formatted date, time, and query string
 		return `Date: ${dateStr}, Time: ${timeStr}<br>${queryParamsText}`;
 	}
 </script>
 
 <button class="btn btn-primary" on:click={downloadPDF}>Download PDF</button>
 
-<!-- Loading overlay -->
 <div class="loading-overlay {loadingPDF ? 'show' : ''}">
 	<div class="spinner-border text-light" role="status">
 		<span class="visually-hidden">Loading...</span>
 	</div>
 	<div class="loading-text">Preparing PDF...</div>
 </div>
+
 
 <style>
 	.loading-overlay {
